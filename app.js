c4b99acb990e2d0d718e5ce1383e3635969ae2ca
Jerry Huang <jerry_huang@trend.com.tw>
2017-02-07 23:11:12 +0800

    Added part of linebot function

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express'),
   app = express(),
   router = express.Router(),
   bodyParser = require('body-parser'),
-  sugar = require('sugar'),
   errorHandler = require('errorhandler'),
   compression = require('compression'),
   http = require('http').Server(app),
@@ -10,8 +9,13 @@ var express = require('express'),
   cluster = require('cluster'),
   os = require('os'),
   readConfig = require('read-config'),
-  config = readConfig('./config/default.json');
-
+  config = readConfig('./config/default.json'),
+  linebot = require('linebot');
+//=================================================================================================
+//routes import 
+//================================================================================================
+var movietime = require('./routes/movietime');
+  
 // ================================================================================================
 // set working directory is this script basedir
 // ===============================================================================================+
@@ -27,11 +31,97 @@ require('./models/db.js').connect(function (err) {
   }
 });
 
-// =======================================================================================================
+//=================================================================================================
+//line bot setting
+//=================================================================================================
+var bot = linebot({
+  channelId: config.linebot.ChannelId,
+  channelSecret: config.linebot.ChannelSecret,
+  channelAccessToken: config.linebot.ChannelAccessToken
+});
+
+var linebotParser = bot.parser();
+
+app.post('/', linebotParser); 
+
+var exStatus = 0;
+
+bot.on('message', function (event) {
+        if (event.message.text === '看電影') {
+	   exStatus = 1; 
+           console.log("看電影");
+        } else if (event.message.text === '找戲院') {
+           exStatus = 2;
+           console.log("找戲院");
+        } else if (exStatus === 1) {
+	  movietime.getMovieTimeAPI({movie: event.message.text}, function (result) {
+            if (result.length === 0) {
+              event.reply("查無資料").then(function (data) {
+                exStatus = 0;
+		console.log('Success', data);
+ 	      }).catch(function (error) {
+                exStatus = 0;
+	       console.log('Error', error);
+	      });
+            } else {
+              result.forEach(function (movie) {
+                var movie_info = '片名:' + movie.name_zh + '(' + movie.name_en + ')\n' + '上映日期:' + movie.release;
+                event.reply(movie_info).then(function (data) {
+		  console.log('Success', data);
+ 	        }).catch(function (error) {
+	          console.log('Error', error);
+	        });
+                movie.theater.forEach(function (theater) {
+                  var theater_info = '片名:' + theater.name + '\n電話:' + theater.tel + '\n' + '時刻表:' + theater.time;
+                  event.reply(theater_info).then(function (data) {
+		    console.log('Success', data);
+ 	          }).catch(function (error) {
+	            console.log('Error', error);
+	          });
+                }); 
+                exStatus = 0;
+              });
+            }
+          });
+        } else if (exStatus === 2) {
+          movietime.getTheaterMoviesAPI({theater: event.message.text}, function (result) {
+            if (result.length === 0) {
+              event.reply("查無資料").then(function (data) {
+                exStatus = 0;
+		console.log('Success', data);
+ 	      }).catch(function (error) {
+               exStatus = 0;
+	       console.log('Error', error);
+	      });
+            } else {
+             result.forEach(function (theater) {
+               event.reply(JSON.stringify(theater)).then(function (data) {
+		  console.log('Success', data);
+ 	        }).catch(function (error) {
+	         console.log('Error', error);
+	        });
+                exStatus = 0;
+              });
+            }
+          });
+        } else {
+          var defaultText = '請輸入: \n 1．"看電影" 或 "找戲院"\n2. 再輸入您要看的電影名稱或戲院名稱'
+    	  event.reply(defaultText).then(function (data) {
+                exStatus = 0;
+		console.log('Success', data);
+	  }).catch(function (error) {
+                exStatus = 0;
+		console.log('Error', error);
+	  });       
+        }
+});
+
+// =================================================================================================
 // configuration
-// =======================================================================================================
+// =================================================================================================
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 app.use(compression());
+
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
 // parse some custom thing into a Buffer
@@ -41,13 +131,12 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 app.use(bodyParser.text({ type: 'text/html' }))
 //app.use(log4js.connectLogger(log4js.getLogger("http"), {level: 'auto'}));
 app.use(errorHandler());
-//app.use(configstore.all.ctxPath, router);
+
 app.use(router);
 
 // ===================================================
 // routes
 // ===================================================
-var movietime = require('./routes/movietime');
 
 router.use(function (req, res, next) {
   var error = '',
--- routes/movietime.js
+++ routes/movietime.js
@@ -49,6 +49,54 @@ var getMovieTime = function (req, res) {
   });
 };
 
+var getMovieTimeAPI = function (data, callback) {
+  var movie = data.movie,
+    jsonResult = [];
+  
+  movieTimeModel.getMovieTime({
+      $or: [
+       {name_zh: new RegExp(movie, "i")},
+       {name_en: new RegExp(movie, 'i')}
+      ]
+      }, function (json) {
+    if (json !== null) {
+      var movie_info = {
+        name_zh: json[0].name_zh,
+        name_en: json[0].name_en,
+        release: json[0].release,
+        theater: []
+      };
+      json.forEach(function (movie) {
+         var theater = {
+         };
+        if (movie_info.name_zh === movie.name_zh) {
+          theater.name = movie.theater_info.name;
+          theater.tel = movie.theater_info.tel;
+          theater.time = movie.movie_time;
+          movie_info.theater.push(theater);
+        } else {
+          jsonResult.push(movie_info);
+          movie_info = {
+            name_zh: movie.name_zh,
+            name_en: movie.name_en,
+            release: movie.release,
+            theater: []
+          }; 
+          theater.theater.name = movie.theater_info.name;
+          theater.theater.tel = movie.theater_info.tel;
+          theater.theater.time = movie.movie_time;
+          movie_info.theater.push(theater_info);
+        } 
+      });
+      jsonResult.push(movie_info);
+      callback(jsonResult);
+    } else {
+      callback([]);
+    }
+  });
+};
+
+
 var getTheaterMovies = function (req, res) {
   var theater = req.param('theater'),
     jsonResult = []; 
@@ -94,6 +142,52 @@ var getTheaterMovies = function (req, res) {
   });
 };
 
+var getTheaterMoviesAPI = function (data, callback) {
+  var theater = data.theater,
+    jsonResult = []; 
+  movieTimeModel.getTheaterMovies({
+    "theater_info.name": new RegExp(theater, "i")
+      }, function (json) {
+    if (json !== null) {
+      var theater_info = {
+        name: json[0].theater_info.name,
+        tel: json[0].theater_info.tel,
+        area: json[0].theater_info.area,
+        movies: []
+      };
+      json.forEach(function (theater) {
+         var movie = {
+         };
+        if (theater_info.name === theater.theater_info.name) {
+          movie.name_zh = theater.name_zh;
+          movie.name_en = theater.name_en;
+          movie.release = theater.release;
+          movie.time = theater.movie_time;
+          theater_info.movies.push(movie);
+        } else {
+          jsonResult.push(theater_info);
+          theater_info = {
+            name: theater.theater_info.name,
+            tel: theater.theater_info.tel,
+            area: theater.theater_info.area,
+            movies: []
+          }; 
+          movie.name_zh = theater.name_zh;
+          movie.name_en = theater.name_en;
+          movie.release = theater.release;
+          movie.time = theater.movie_time;
+          theater_info.movies.push(movie);
+        } 
+      });
+      jsonResult.push(theater_info);
+      callback(jsonResult);
+    } else {
+      callback([]);
+    }
+  });
+};
+
+
 var createMovie = function (req, res) {
   //TO-DO;
 };
@@ -111,5 +205,7 @@ module.exports = {
   getTheaterMovies: getTheaterMovies,
   createMovie: createMovie,
   deleteMovie: deleteMovie,
-  updateMovie: updateMovie
+  updateMovie: updateMovie,
+  getMovieTimeAPI: getMovieTimeAPI,
+  getTheaterMoviesAPI: getTheaterMoviesAPI
 };
